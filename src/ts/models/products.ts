c2aeab7509383628bd33ebdaf3dd7e76de29c6d2
Metin <metinmayi@gmail.com>
2021-12-02 12:47:31 +0100

    Created the products

---
+++ src/ts/models/products.ts
@@ -0,0 +1,110 @@
+export let products = [
+	{
+		name: "Cat Sandwich",
+		url: "https://i.ibb.co/1fKFDcB/image.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Cat Tub",
+		url: "https://i.ibb.co/TBv3gf8/1.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Generous Dog",
+		url: "https://i.ibb.co/B4BfBWD/2.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Depressed Dog",
+		url: "https://i.ibb.co/pWVBqmn/3.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Exotic Dog",
+		url: "https://i.ibb.co/vzgHDBr/4.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Silent Dog",
+		url: "https://i.ibb.co/fxJVc9v/5.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Massage Dog",
+		url: "https://i.ibb.co/TYj56Fs/6.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Clean Dog",
+		url: "https://i.ibb.co/WF264XK/7.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Wrinkly Depressed Dog",
+		url: "https://i.ibb.co/N30FYMP/8.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Hipster Dog",
+		url: "https://i.ibb.co/mSsk0C1/9.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Fast Rabbit",
+		url: "https://i.ibb.co/6tLQKsB/10.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+	{
+		name: "Dog Dealer",
+		url: "https://i.ibb.co/dJq0z70/11.jpg",
+		price: {
+			s: 10,
+			m: 20,
+			l: 30,
+		},
+	},
+];
