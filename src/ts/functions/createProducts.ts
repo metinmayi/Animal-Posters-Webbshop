635baf46b08e1f4207a1650d6ad91beba28dfd10
Metin <metinmayi@gmail.com>
2021-12-04 21:08:39 +0100

    creteProducts function working properly

--- src/ts/functions/createProducts.ts
+++ src/ts/functions/createProducts.ts
@@ -1,20 +1,29 @@
 import { products } from "../models/products";
 
 export function createProducts() {
+	const mainProducts: HTMLDivElement = document.getElementById(
+		"mainProducts"
+	) as HTMLDivElement;
 	products.forEach((e) => {
-		let newDiv: HTMLDivElement = document.createElement("div");
+		let productsDiv: HTMLDivElement = document.createElement("div");
+		let imgWrapperDiv: HTMLDivElement = document.createElement("div");
+		let emptyHeartDiv: HTMLDivElement = document.createElement("div");
 		let newImg: HTMLImageElement = document.createElement("img");
-		let newH4: HTMLHeadingElement = document.createElement("h4");
-		let newH6: HTMLHeadingElement = document.createElement("h6");
+		let p1: HTMLHeadingElement = document.createElement("p");
+		let p2: HTMLHeadingElement = document.createElement("p");
 
+		productsDiv.className = "product";
+		imgWrapperDiv.className = "imgWrapper";
 		newImg.src = e.url;
-		console.log(e.url);
-		newH4.innerHTML = e.name;
-		newH6.innerHTML = `Fr ${e.price.s}kr`;
+		p1.innerHTML = e.name;
+		p1.id = "productTitle";
+		p2.innerHTML = `Fr. ${e.price.s}kr`;
 
-		document.body.appendChild(newDiv);
-		newDiv.appendChild(newImg);
-		newDiv.appendChild(newH4);
-		newDiv.appendChild(newH6);
+		mainProducts.appendChild(productsDiv);
+		productsDiv.appendChild(imgWrapperDiv);
+		imgWrapperDiv.appendChild(emptyHeartDiv);
+		imgWrapperDiv.appendChild(newImg);
+		productsDiv.appendChild(p1);
+		productsDiv.appendChild(p2);
 	});
 }
