d77b63df80beba2a3f90cc56c7a6b60003c31ebc
Metin <metinmayi@gmail.com>
2021-12-06 14:04:21 +0100

    Dynamically updates the price

--- src/ts/functions/displayProductsModal.ts
+++ src/ts/functions/displayProductsModal.ts
@@ -1,19 +1,9 @@
 import { products } from "../models/products";
 
 export function displayProductModals(Id: number) {
-	console.log("Modal was clicked!");
-	//Opens the Modal
+	//#region Declarations
 	let modalContainer: HTMLDivElement = document.getElementById(
 		"modalContainer"
-	) as HTMLDivElement;
-	modalContainer.className = "showProductModal";
-
-	//Produces the modal product based on which product was clicked on the website.
-	let modalBody: HTMLDivElement = document.getElementById(
-		"modalBody"
-	) as HTMLDivElement;
-	let modalImgWrapper: HTMLDivElement = document.getElementById(
-		"modalImgWrapper"
 	) as HTMLDivElement;
 	let modalImg: HTMLImageElement = document.getElementById(
 		"modalImg"
@@ -24,21 +14,54 @@ export function displayProductModals(Id: number) {
 	let modalTotalPrice: HTMLParagraphElement = document.getElementById(
 		"modalTotal"
 	) as HTMLParagraphElement;
-
-	// let newImg: HTMLImageElement = document.createElement("img");
-
-	modalImg.src = products[Id].url;
-	modalTitle.innerText = products[Id].name;
-	modalTotalPrice.innerText = `Totalbelopp: ${products[Id].price.s}kr`;
-
-	// modalImgWrapper.appendChild(newImg);
-
-	//Closes the Modal
+	let sizeInput: HTMLSelectElement = document.getElementById(
+		"productModalSize"
+	) as HTMLSelectElement;
+	let amountInput: HTMLInputElement = document.getElementById(
+		"productModalAmount"
+	) as HTMLInputElement;
 	let closeProductsModal: HTMLButtonElement = document.getElementById(
 		"closeProductsModal"
 	) as HTMLButtonElement;
+	//#endregion
+
+	//Opens the Modal
+	modalContainer.className = "showProductModal";
+
+	//Produces the modal product based on which product was clicked on the website.
+	modalImg.src = products[Id].url;
+	modalTitle.innerText = products[Id].name;
+	modalTotalPrice.innerText = `Totalbelopp: ${products[Id].price.s}kr`;
+	//Calculates and updates the totalprice based on product size and amount of products.
+	let totalprice: number;
+	sizeInput.addEventListener("input", (e) => {
+		if (!amountInput.value) return;
+		if (sizeInput.value == "s")
+			totalprice = parseInt(amountInput.value) * products[Id].price.s;
+		if (sizeInput.value == "m")
+			totalprice = parseInt(amountInput.value) * products[Id].price.m;
+		if (sizeInput.value == "l")
+			totalprice = parseInt(amountInput.value) * products[Id].price.l;
+		modalTotalPrice.innerText = `Totalbelopp: ${totalprice}kr`;
+		console.log("Registered");
+	});
+	amountInput.addEventListener("input", (e) => {
+		if (!amountInput.value) return;
+		if (sizeInput.value == "s")
+			totalprice = parseInt(amountInput.value) * products[Id].price.s;
+		if (sizeInput.value == "m")
+			totalprice = parseInt(amountInput.value) * products[Id].price.m;
+		if (sizeInput.value == "l")
+			totalprice = parseInt(amountInput.value) * products[Id].price.l;
+		modalTotalPrice.innerText = `Totalbelopp: ${totalprice}kr`;
+		console.log("Registered");
+	});
+
+	console.log(sizeInput.value, amountInput.value, totalprice);
+
+	//Closes the Modal
+
 	closeProductsModal.addEventListener("click", (e) => {
 		modalContainer.className = "";
-		console.log("Button was clicked!");
 	});
 }
