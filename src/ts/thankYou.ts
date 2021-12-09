7ab80817e11e658c4ae60b6c8ce254469ed0328d
”OliviaAndersson” <olivia.moaandersson@gmail.com>
2021-12-09 13:32:55 +0100

    Change hero, ts-file working & styles

--- src/ts/thankYou.ts
+++ src/ts/thankYou.ts
@@ -1,1 +1,13 @@
-window.onload = function () {};
+window.onload = function () {
+  let heading: HTMLHeadingElement = document.createElement("h1");
+  heading.innerHTML = "Tack för din beställning!";
+
+  let totalOrder: HTMLParagraphElement = document.createElement("p");
+  totalOrder.innerHTML =
+    "Din beställning på " +
+    window.localStorage.length +
+    " stycken posters är beställd och skickas snart från vårat lager!";
+
+  document.getElementById("order-details").appendChild(heading);
+  document.getElementById("order-details").appendChild(totalOrder);
+};
