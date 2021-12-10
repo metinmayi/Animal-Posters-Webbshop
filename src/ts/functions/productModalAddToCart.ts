22e9931d89f95c8931dcc3918bc3d3d87c10b068
Metin <metinmayi@gmail.com>
2021-12-10 08:48:38 +0100

    Working on it

--- src/ts/functions/productModalAddToCart.ts
+++ src/ts/functions/productModalAddToCart.ts
@@ -19,30 +19,83 @@ export function productModalAddToCart() {
   ) as HTMLSelectElement;
   //#endregion
   addButton.addEventListener("click", (e) => {
-    console.log(titleIdentifier.innerHTML + "!");
-    let regex: RegExp = new RegExp(titleIdentifier.innerHTML);
-    let correctProduct;
+    let incomingProduct: Iproducts;
+    let productName: string = titleIdentifier.innerHTML;
+    let match: boolean;
 
-    products.forEach((e) => {
-      regex.test(e.name) ? (correctProduct = e) : null;
+    //Finds the correct products by checking which one has the same name as the Modal title
+    products.forEach((product) => {
+      productName == product.name ? (incomingProduct = product) : null;
     });
-    console.log(correctProduct);
-
+    //Sets the size that the user selected.
+    if (size.value == "s") incomingProduct.small = true;
+    if (size.value == "m") incomingProduct.medium = true;
+    if (size.value == "l") incomingProduct.large = true;
+    //If there´s no list in LS, it creates one
     if (!localStorage.getItem("cartList")) {
       localStorage.setItem("cartList", "[]");
     }
 
     let cartList: string = localStorage.getItem("cartList");
-    let cartListArray: Iproducts[] = JSON.parse(cartList);
+    let localStorageArray: Iproducts[] = JSON.parse(cartList);
 
-    if (size.value == "s") correctProduct.small = true;
-    if (size.value == "m") correctProduct.medium = true;
-    if (size.value == "l") correctProduct.large = true;
-    for (let x = 0; x < parseInt(amountOfProducts.value); x++) {
-      cartListArray.push(correctProduct);
+    //If theres no item in the LS list, it will only add a value and eventually get pushed.
+    if (localStorageArray.length == 0) {
+      incomingProduct.amount = parseInt(amountOfProducts.value);
+      localStorageArray.push(incomingProduct);
+      console.log("Length was 0, pushed item instantly");
     }
-    let cartListArrayStringify: string = JSON.stringify(cartListArray);
-    localStorage.setItem("cartList", cartListArrayStringify);
+    // If there are items in the list. We want to check for matches. Upon matching, edit a products ".amount" and push, rather than pushing a new object.
+    else {
+      localStorageArray.forEach((localStorageItem) => {
+        if (localStorageItem.name == incomingProduct.name) {
+          // console.log("Got a namematch");
+          if (localStorageItem.small == true && incomingProduct.small == true) {
+            console.log("Got a name AND a small size match!");
+            // localStorageItem.amount += parseInt(amountOfProducts.value);
+            match = true;
+            console.log("Same name and same small size. Set match to true");
+          } else if (
+            localStorageItem.medium == true &&
+            incomingProduct.medium == true
+          ) {
+            console.log("Same name and same medium size, set match to true.");
+            // incomingProduct.amount++;
+            match = true;
+          } else if (
+            localStorageItem.large == true &&
+            incomingProduct.large == true
+          ) {
+            console.log("Same name and same medium size, set match to true.");
+            // incomingProduct.amount++;
+            match = true;
+          }
+          // else {
+          //   console.log("Not same name, not same size. Set match to false");
+          //   // incomingProduct.amount = parseInt(amountOfProducts.value);
+          //   // localStorageArray.push(incomingProduct);
+          //   match = false;
+          // }
+        }
+        if (localStorageItem.name != incomingProduct.name) {
+          console.log("Entirely new name, set match to false");
+          match = false;
+        }
+      });
+    }
+    if (match == true) {
+      incomingProduct.amount += parseInt(amountOfProducts.value);
+    }
+    if (match == false) {
+      incomingProduct.amount = parseInt(amountOfProducts.value);
+      localStorageArray.push(incomingProduct);
+    }
+
+    let localStorageArrayStringify: string = JSON.stringify(localStorageArray);
+    localStorage.setItem("cartList", localStorageArrayStringify);
+    incomingProduct.small = false;
+    incomingProduct.medium = false;
+    incomingProduct.large = false;
 
     //Closes modal
     modalContainer.className = "";
