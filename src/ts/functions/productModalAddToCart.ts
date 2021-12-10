94246b2cdf8ef963f2782a9650177273c4a977fc
Metin <metinmayi@gmail.com>
2021-12-10 08:48:38 +0100

    Working on it

--- src/ts/functions/productModalAddToCart.ts
+++ src/ts/functions/productModalAddToCart.ts
@@ -27,49 +27,82 @@ export function productModalAddToCart() {
   ) as HTMLSelectElement;
   //#endregion
   addButton.addEventListener("click", (e) => {
-    //Takes the title of the Modal. Checks our list of products(products[]) and finds the one that has the same name as the Modal title. This it how we identify
-    //which product was clicked.
     let incomingProduct: Iproducts;
     let productName: string = titleIdentifier.innerHTML;
+    let match: boolean;
+
+    //Finds the correct products by checking which one has the same name as the Modal title
     products.forEach((product) => {
       productName == product.name ? (incomingProduct = product) : null;
     });
-    //Creates a new product based on the incomingProdcut(Iproduct), the size and amount values that the user put in.
-    let newProduct = new StorageProduct(
-      incomingProduct,
-      size.value,
-      parseInt(amountOfProducts.value)
-    );
-
-    //All CODE BELOW IS MANIPULATING THE LOCALSTORAGE.
-    //Checks for an existing LocalStorage list. If there is none, it creates an empty array.
+    //Sets the size that the user selected.
+    if (size.value == "s") incomingProduct.small = true;
+    if (size.value == "m") incomingProduct.medium = true;
+    if (size.value == "l") incomingProduct.large = true;
+    //If there´s no list in LS, it creates one
     if (!localStorage.getItem("cartList")) {
       localStorage.setItem("cartList", "[]");
     }
     let cartList: string = localStorage.getItem("cartList");
-    let localStorageArray: StorageProduct[] = JSON.parse(cartList);
+    let localStorageArray: Iproducts[] = JSON.parse(cartList);
 
-    //Compares our newProduct with the ones that exist in the localStorage. If it finds a match, it sets the matchIndex to the index of the found match.
-    let matchIndex: number;
-    localStorageArray.forEach((storageItem, storageItemIndex) => {
-      if (
-        JSON.stringify(storageItem.Iproduct) ===
-          JSON.stringify(newProduct.Iproduct) &&
-        storageItem.size.toString() === newProduct.size.toString()
-      ) {
-        matchIndex = storageItemIndex;
-      }
-    });
-
-    //If a match was found previously, it only changes the amount of the already existing object. Else, it pushes a new object to the array.
-    if (matchIndex >= 0) {
-      localStorageArray[matchIndex].amount += parseInt(amountOfProducts.value);
-    } else {
-      localStorageArray.push(newProduct);
+    //If theres no item in the LS list, it will only add a value and eventually get pushed.
+    if (localStorageArray.length == 0) {
+      incomingProduct.amount = parseInt(amountOfProducts.value);
+      localStorageArray.push(incomingProduct);
+      console.log("Length was 0, pushed item instantly");
+    }
+    // If there are items in the list. We want to check for matches. Upon matching, edit a products ".amount" and push, rather than pushing a new object.
+    else {
+      localStorageArray.forEach((localStorageItem) => {
+        if (localStorageItem.name == incomingProduct.name) {
+          // console.log("Got a namematch");
+          if (localStorageItem.small == true && incomingProduct.small == true) {
+            console.log("Got a name AND a small size match!");
+            // localStorageItem.amount += parseInt(amountOfProducts.value);
+            match = true;
+            console.log("Same name and same small size. Set match to true");
+          } else if (
+            localStorageItem.medium == true &&
+            incomingProduct.medium == true
+          ) {
+            console.log("Same name and same medium size, set match to true.");
+            // incomingProduct.amount++;
+            match = true;
+          } else if (
+            localStorageItem.large == true &&
+            incomingProduct.large == true
+          ) {
+            console.log("Same name and same medium size, set match to true.");
+            // incomingProduct.amount++;
+            match = true;
+          }
+          // else {
+          //   console.log("Not same name, not same size. Set match to false");
+          //   // incomingProduct.amount = parseInt(amountOfProducts.value);
+          //   // localStorageArray.push(incomingProduct);
+          //   match = false;
+          // }
+        }
+        if (localStorageItem.name != incomingProduct.name) {
+          console.log("Entirely new name, set match to false");
+          match = false;
+        }
+      });
+    }
+    if (match == true) {
+      incomingProduct.amount += parseInt(amountOfProducts.value);
+    }
+    if (match == false) {
+      incomingProduct.amount = parseInt(amountOfProducts.value);
+      localStorageArray.push(incomingProduct);
     }
 
     let localStorageArrayStringify: string = JSON.stringify(localStorageArray);
     localStorage.setItem("cartList", localStorageArrayStringify);
+    incomingProduct.small = false;
+    incomingProduct.medium = false;
+    incomingProduct.large = false;
 
     //Closes modal
     modalContainer.className = "";
