2d12dda38804c51556284d8d809ee4c6546d9682
”OliviaAndersson” <olivia.moaandersson@gmail.com>
2021-12-10 22:42:25 +0100

    Local storage working & remove button

--- src/ts/functions/createProductsCheckout.ts
+++ src/ts/functions/createProductsCheckout.ts
@@ -1,4 +1,7 @@
 import { create } from "cypress/types/lodash";
+import { Iproducts } from "../models/products";
+let totalPrice: number = 0;
+let momsPrice: number = 0;
 
 export function createProductsCheckout() {
   document.getElementById("shopping-cart").addEventListener("click", () => {
@@ -14,23 +17,20 @@ export function createProductsCheckout() {
     document.getElementById("checkout-dropdown").style.display = "none";
   });
 
-  if (window.localStorage.length == 1) {
-    console.log("fylld");
-    let emptyList: HTMLDivElement = document.createElement("div");
-    emptyList.id = "empty-container";
+  document.getElementById("add-button").addEventListener("click", () => {
+    let cartListString: string = JSON.stringify(listAsObject);
+    console.log(cartListString);
+    localStorage.setItem("cartList", cartListString);
+  });
 
-    let emptyText: HTMLParagraphElement = document.createElement("p");
-    emptyText.id = "empty-text";
-    emptyText.innerHTML =
-      "Din varukorg är tom, tryck på en vara och lägg till den i varukorgen för att fortsätta";
+  let cartListLS: string = localStorage.getItem("cartList");
+  let listAsObject: Iproducts = JSON.parse(cartListLS);
 
-    document
-      .getElementById("checkout-products-container")
-      .appendChild(emptyList);
-    emptyList.appendChild(emptyText);
-  } else {
-    console.log("inte tom");
+  ifElse(listAsObject);
+}
 
+export function ifElse(lista) {
+  if (localStorage.hasOwnProperty("cartList")) {
     //Tar bort de gamla HTML-elementen fär att ge plats åt en uppdaterad lista
     let productsRemove = document.getElementById("checkout-products-container");
     while (productsRemove.firstChild) {
@@ -49,96 +49,143 @@ export function createProductsCheckout() {
       totalRemove.removeChild(totalRemove.firstChild);
     }
 
-    let cartListLS = localStorage.getItem("cartList");
+    createHTML(lista);
+  } else {
+    console.log("tom");
+    let emptyList: HTMLDivElement = document.createElement("div");
+    emptyList.id = "empty-container";
 
-    let listAsObject = JSON.parse(cartListLS);
+    let emptyText: HTMLParagraphElement = document.createElement("p");
+    emptyText.id = "empty-text";
+    emptyText.innerHTML =
+      "Din varukorg är tom, tryck på en vara och lägg till den i varukorgen för att fortsätta";
 
-    console.log(listAsObject);
+    document
+      .getElementById("checkout-products-container")
+      .appendChild(emptyList);
+    emptyList.appendChild(emptyText);
+  }
+}
 
-    let totalPrice: number = 0;
-    let momsPrice: number = 0;
+export function createHTML(listAsObject) {
+  //Skapar HTML efter listan som är lagrad i localStorage
 
-    //Skapar HTML efter listan som är lagrad i localStorage
-    for (let i = 0; i < listAsObject.length; i++) {
-      console.log(listAsObject[i].url);
+  totalPrice = 0;
 
-      let productWrapper: HTMLDivElement = document.createElement("div");
-      productWrapper.className = "checkout-products";
+  for (let i = 0; i < listAsObject.length; i++) {
+    let productWrapper: HTMLDivElement = document.createElement("div");
+    productWrapper.className = "checkout-products";
 
-      let imageWrapper: HTMLDivElement = document.createElement("div");
-      imageWrapper.className = "image-wrapper";
-      imageWrapper.setAttribute(
-        "background-image",
-        `url("${listAsObject[i].url}")`
-      );
+    let imageWrapper: HTMLDivElement = document.createElement("div");
+    imageWrapper.className = "image-wrapper";
+    imageWrapper.setAttribute(
+      "style",
+      `background-image: url("${listAsObject[i].url}")`
+    );
 
-      let textWrapper: HTMLDivElement = document.createElement("div");
-      textWrapper.className = "text-wrapper";
+    let textWrapper: HTMLDivElement = document.createElement("div");
+    textWrapper.className = "text-wrapper";
 
-      let productName: HTMLHeadingElement = document.createElement("h1");
-      productName.id = "name";
-      productName.innerHTML = listAsObject[i].name;
+    let productName: HTMLHeadingElement = document.createElement("h1");
+    productName.id = "name";
+    productName.innerHTML = listAsObject[i].name;
 
-      let productSize: HTMLParagraphElement = document.createElement("p");
-      productSize.id = "size";
+    let productSize: HTMLParagraphElement = document.createElement("p");
+    productSize.id = "size";
 
-      if (listAsObject[i].small === true) {
-        productSize.innerHTML = "Storlek: Small";
-      } else if (listAsObject[i].medium === true) {
-        productSize.innerHTML = "Storlek: Medium";
-      } else {
-        productSize.innerHTML = "Storlek: Large";
-      }
-
-      let productPrice: HTMLParagraphElement = document.createElement("p");
-      productPrice.id = "price";
-      if (listAsObject[i].small === true) {
-        productPrice.innerHTML = listAsObject[i].price.s + " kr";
-      } else if (listAsObject[i].medium === true) {
-        productPrice.innerHTML = listAsObject[i].price.m + " kr";
-      } else {
-        productPrice.innerHTML = listAsObject[i].price.l + " kr";
-      }
-
-      let trash: HTMLElement = document.createElement("i");
-      trash.className = "far fa-trash-alt";
-      trash.id = "trash";
-
-      document
-        .getElementById("checkout-products-container")
-        .appendChild(productWrapper);
-      productWrapper.appendChild(imageWrapper);
-      productWrapper.appendChild(textWrapper);
-
-      textWrapper.appendChild(productName);
-      textWrapper.appendChild(productSize);
-      textWrapper.appendChild(productPrice);
-
-      productWrapper.appendChild(trash);
-
-      //Varje produkts pris adderas för aqtt sen kunna räkna ut totalsumman
-      totalPrice += parseInt(productPrice.innerHTML);
+    if (listAsObject[i].small === true) {
+      productSize.innerHTML = "Storlek: Small";
+    } else if (listAsObject[i].medium === true) {
+      productSize.innerHTML = "Storlek: Medium";
+    } else {
+      productSize.innerHTML = "Storlek: Large";
     }
 
-    momsPrice = 0.12 * totalPrice;
+    let productPrice: HTMLParagraphElement = document.createElement("p");
+    productPrice.id = "price";
+    if (listAsObject[i].small === true) {
+      productPrice.innerHTML = listAsObject[i].price.s + " kr";
+    } else if (listAsObject[i].medium === true) {
+      productPrice.innerHTML = listAsObject[i].price.m + " kr";
+    } else {
+      productPrice.innerHTML = listAsObject[i].price.l + " kr";
+    }
+    let trashDiv: HTMLDivElement = document.createElement("div");
+    trashDiv.id = "trash-div";
 
-    let totalText: HTMLParagraphElement = document.createElement("p");
-    totalText.innerHTML = "Totalsumma:";
+    let trash: HTMLElement = document.createElement("i");
+    trash.className = "far fa-trash-alt";
 
-    let momsText: HTMLParagraphElement = document.createElement("p");
-    momsText.innerHTML = "Varav moms:";
+    document
+      .getElementById("checkout-products-container")
+      .appendChild(productWrapper);
+    productWrapper.appendChild(imageWrapper);
+    productWrapper.appendChild(textWrapper);
 
-    let total: HTMLParagraphElement = document.createElement("p");
-    total.innerHTML = totalPrice.toString() + " kr";
+    textWrapper.appendChild(productName);
+    textWrapper.appendChild(productSize);
+    textWrapper.appendChild(productPrice);
 
-    let moms: HTMLParagraphElement = document.createElement("p");
-    moms.innerHTML = momsPrice.toString() + " kr";
+    productWrapper.appendChild(trashDiv);
+    trashDiv.appendChild(trash);
 
-    document.getElementById("total-price").appendChild(totalText);
-    document.getElementById("total-price").appendChild(total);
-    document.getElementById("moms-price").appendChild(momsText);
-    document.getElementById("moms-price").appendChild(moms);
+    //Varje produkts pris adderas för aqtt sen kunna räkna ut totalsumman
+    totalPrice += parseInt(productPrice.innerHTML);
+
+    trashDiv.addEventListener("click", () => {
+      momsPrice = 0;
+      console.log(listAsObject[i]);
+      // let indexRemove = listAsObject.indexOf(listAsObject[i]);
+      listAsObject.splice(i, 1);
+
+      console.log(listAsObject);
+
+      let deleteList = document.getElementById("checkout-products-container");
+
+      while (deleteList.firstChild) {
+        deleteList.removeChild(deleteList.firstChild);
+      }
+
+      let deleteMomsList = document.getElementById("moms-price");
+
+      while (deleteMomsList.firstChild) {
+        deleteMomsList.removeChild(deleteMomsList.firstChild);
+      }
+      let deletTotalList = document.getElementById("total-price");
+
+      while (deletTotalList.firstChild) {
+        deletTotalList.removeChild(deletTotalList.firstChild);
+      }
+
+      createHTML(listAsObject);
+
+      let cartListString: string = JSON.stringify(listAsObject);
+      console.log(cartListString);
+      localStorage.setItem("cartList", cartListString);
+    });
   }
+  createPrice(totalPrice);
+}
 
-  // }
+function createPrice(totalPrice) {
+  momsPrice = 0.12 * totalPrice;
+
+  momsPrice = Math.round(momsPrice);
+
+  let totalText: HTMLParagraphElement = document.createElement("p");
+  totalText.innerHTML = "Totalsumma:";
+
+  let momsText: HTMLParagraphElement = document.createElement("p");
+  momsText.innerHTML = "Varav moms:";
+
+  let total: HTMLParagraphElement = document.createElement("p");
+  total.innerHTML = totalPrice.toString() + " kr";
+
+  let moms: HTMLParagraphElement = document.createElement("p");
+  moms.innerHTML = momsPrice.toString() + " kr";
+
+  document.getElementById("total-price").appendChild(totalText);
+  document.getElementById("total-price").appendChild(total);
+  document.getElementById("moms-price").appendChild(momsText);
+  document.getElementById("moms-price").appendChild(moms);
 }
