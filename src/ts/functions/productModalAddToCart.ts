391aa420d2466d665e010dea7ee36bc318d9f62d
Metin <metinmayi@gmail.com>
2021-12-10 17:12:57 +0100

    Products are now correctly added to the cart. Increases amount of existing items and pushes new items.

--- src/ts/functions/productModalAddToCart.ts
+++ src/ts/functions/productModalAddToCart.ts
@@ -27,30 +27,30 @@ export function productModalAddToCart() {
   ) as HTMLSelectElement;
   //#endregion
   addButton.addEventListener("click", (e) => {
+    //Takes the title of the Modal. Checks our list of products(products[]) and finds the one that has the same name as the Modal title. This it how we identify
+    //which product was clicked.
     let incomingProduct: Iproducts;
     let productName: string = titleIdentifier.innerHTML;
-    let match: boolean = true;
-
-    //Finds the correct products by checking which one has the same name as the Modal title
     products.forEach((product) => {
       productName == product.name ? (incomingProduct = product) : null;
     });
-
+    //Creates a new product based on the incomingProdcut(Iproduct), the size and amount values that the user put in.
     let newProduct = new StorageProduct(
       incomingProduct,
       size.value,
       parseInt(amountOfProducts.value)
     );
-    //Sets the size that the user selected.
-    //If there´s no list in LS, it creates one
+
+    //All CODE BELOW IS MANIPULATING THE LOCALSTORAGE.
+    //Checks for an existing LocalStorage list. If there is none, it creates an empty array.
     if (!localStorage.getItem("cartList")) {
       localStorage.setItem("cartList", "[]");
     }
     let cartList: string = localStorage.getItem("cartList");
     let localStorageArray: StorageProduct[] = JSON.parse(cartList);
 
+    //Compares our newProduct with the ones that exist in the localStorage. If it finds a match, it sets the matchIndex to the index of the found match.
     let matchIndex: number;
-
     localStorageArray.forEach((storageItem, storageItemIndex) => {
       if (
         JSON.stringify(storageItem.Iproduct) ===
@@ -58,12 +58,10 @@ export function productModalAddToCart() {
         storageItem.size.toString() === newProduct.size.toString()
       ) {
         matchIndex = storageItemIndex;
-        console.log("Match found");
-        console.log(JSON.stringify(newProduct.Iproduct));
-        console.log(newProduct.size.toString());
       }
     });
 
+    //If a match was found previously, it only changes the amount of the already existing object. Else, it pushes a new object to the array.
     if (matchIndex >= 0) {
       localStorageArray[matchIndex].amount += parseInt(amountOfProducts.value);
     } else {
@@ -76,7 +74,7 @@ export function productModalAddToCart() {
     //Closes modal
     modalContainer.className = "";
 
-    //Uppdaterar listan och startar om funktionen för att skapa nya HTML-element i dropdownen
-    // createProductsCheckout();
+    //Calls the function that updates the products in the Checkout dropdown.
+    createProductsCheckout();
   });
 }
