362b21e1f23da20e34873dc6ce84ab71d00420e5
”OliviaAndersson” <olivia.moaandersson@gmail.com>
2021-12-13 09:38:48 +0100

    added dropdown to all pages

--- src/ts/indexpage.ts
+++ src/ts/indexpage.ts
@@ -1,10 +1,14 @@
 import { activateHamburgerMenu } from "./functions/activateHamburgerMenu";
-import { createProducts } from "./functions/createProducts";
+// import { createProducts } from "./functions/createProducts";
 import { randomQuotes } from "./functions/randomQuotes";
+import { createProductsCheckout } from "./functions/createProductsCheckout";
 
 window.onload = function () {
-	//Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
-	activateHamburgerMenu();
-	//Slumpar fram citat om våra produkter, som står på index-sidan, byts vid refresh
-	randomQuotes();
+  //Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
+  activateHamburgerMenu();
+  //Slumpar fram citat om våra produkter, som står på index-sidan, byts vid refresh
+  randomQuotes();
+
+  //Aktiverar och skapar HTML i checkout-dropdownen
+  createProductsCheckout();
 };
--- src/ts/shoppingCartPage.ts
+++ src/ts/shoppingCartPage.ts
@@ -1,8 +1,13 @@
 import { activateHamburgerMenu } from "./functions/activateHamburgerMenu";
 import { displayCart } from "./functions/displayCart";
+import { createProductsCheckout } from "./functions/createProductsCheckout";
+
 window.onload = () => {
   //Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
   activateHamburgerMenu();
   // Funktionen visar vad du har lagt i varukorgen
   displayCart();
+
+  //Aktiverar och skapar HTML i checkout-dropdownen
+  createProductsCheckout();
 };
