d205b263d51ab59d9c340caa44b7fd72e2a2ca0d
Metin <metinmayi@gmail.com>
2021-12-14 09:54:24 +0100

    Wishlist now works on all pages

--- src/ts/checkoutPage.ts
+++ src/ts/checkoutPage.ts
@@ -1,8 +1,12 @@
 import { activateHamburgerMenu } from "./functions/activateHamburgerMenu";
 import { displayCheckout } from "./functions/dispayCheckout";
+import { refreshWishList, wishList } from "./functions/wishListDisplays";
 window.onload = () => {
   //Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
   activateHamburgerMenu();
   // Funktionen visar vad du har lagt i varukorgen i checkout
   displayCheckout();
+  //Shows wishList
+  wishList();
+  refreshWishList();
 };
