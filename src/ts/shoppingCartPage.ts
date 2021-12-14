187d2c73fbc291d4ce5e11f32a541f95e43450b8
Sofie Blom <ssofieblom@hotmail.com>
2021-12-14 10:01:13 +0100

    removed a comment

--- src/ts/shoppingCartPage.ts
+++ src/ts/shoppingCartPage.ts
@@ -3,15 +3,15 @@ import { createProductsCheckout } from "./functions/createProductsCheckout";
 import { displayCart } from "./functions/displayCart";
 import { wishList, refreshWishList } from "./functions/wishListDisplays";
 window.onload = () => {
-	//Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
-	activateHamburgerMenu();
-	// Funktionen visar vad du har lagt i varukorgen
-	displayCart();
+  //Funktionen aktiverar och avaktiverar hamburgarmenyn med hjälp av "click"
+  activateHamburgerMenu();
+  // Funktionen visar vad du har lagt i varukorgen
+  displayCart();
 
-	//Skapar eventlisteners för att öppna/stänga wishlist
-	wishList();
-	//Uppdaterar wishlist innehållet
-	refreshWishList();
-	//Aktiverar och skapar HTML i checkout-dropdownen
-	createProductsCheckout();
+  //Skapar eventlisteners för att öppna/stänga wishlist
+  wishList();
+  //Uppdaterar wishlist innehållet
+  refreshWishList();
+  //Aktiverar och skapar HTML i checkout-dropdownen
+  createProductsCheckout();
 };
