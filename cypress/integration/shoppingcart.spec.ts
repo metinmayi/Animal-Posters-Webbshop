93094a30f34bf868eb868be43c00bbf0b26e227a
Sofie Blom <ssofieblom@hotmail.com>
2021-12-15 15:16:35 +0100

    testing testing

--- cypress/integration/shoppingcart.spec.ts
+++ cypress/integration/shoppingcart.spec.ts
@@ -1,6 +1,6 @@
 describe("Testing shoppingcart page", () => {
   it("Should see an emty shoppingcart when localstorage is emty", () => {
     cy.visit("http://localhost:1234/shoppingcart.html");
-    // cy.get("ul li").should("have.length", 0);
+    cy.get("#ul li").should("have.length", 0);
   });
 });
