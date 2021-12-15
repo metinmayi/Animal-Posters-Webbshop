b0ca1e9346da1a6a58623fe5462226e5cbcc7ab3
”OliviaAndersson” <olivia.moaandersson@gmail.com>
2021-12-15 14:41:21 +0100

    amount on thankyou is now correct

--- src/ts/thankYou.ts
+++ src/ts/thankYou.ts
@@ -1,13 +1,28 @@
+import { StorageProduct } from "../ts/models/products";
+
 window.onload = function () {
-	let heading: HTMLHeadingElement = document.createElement("h1");
-	heading.innerHTML = "Tack för din beställning!";
+  let cartListLS: string = localStorage.getItem("cartList");
+  let amountList: StorageProduct[] = JSON.parse(cartListLS);
 
-	let totalOrder: HTMLParagraphElement = document.createElement("p");
-	totalOrder.innerHTML =
-		"Din beställning på " +
-		window.localStorage.length +
-		" stycken posters är beställd och skickas snart från vårat lager!";
+  let heading: HTMLHeadingElement = document.createElement("h1");
+  heading.innerHTML = "Tack för din beställning!";
+  let totalOrder: HTMLParagraphElement = document.createElement("p");
+  let totalOrderNumber: number = 0;
 
-	document.getElementById("order-details").appendChild(heading);
-	document.getElementById("order-details").appendChild(totalOrder);
+  if (amountList === null) {
+    totalOrder.innerHTML =
+      "Din beställning på 0 stycken posters är beställd och skickas snart från vårat lager!";
+  } else {
+    for (let i = 0; i < amountList.length; i++) {
+      totalOrderNumber += amountList[i].amount;
+    }
+
+    totalOrder.innerHTML =
+      "Din beställning på " +
+      totalOrderNumber +
+      " stycken posters är beställd och skickas snart från vårat lager!";
+  }
+
+  document.getElementById("order-details").appendChild(heading);
+  document.getElementById("order-details").appendChild(totalOrder);
 };
