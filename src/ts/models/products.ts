ce65ada67e47a81bd025d06d7cef3a2b3df32d46
Sofie Blom <ssofieblom@hotmail.com>
2021-12-16 18:12:26 +0100

    assets and links

--- src/ts/models/products.ts
+++ src/ts/models/products.ts
@@ -1,146 +1,146 @@
 export let products: Iproducts[] = [
-	{
-		name: "Merge Conflicts",
-		url: "https://i.ibb.co/1fKFDcB/image.jpg",
-		wishList: false,
-		price: {
-			s: 10,
-			m: 20,
-			l: 30,
-		},
-	},
-	{
-		name: "Parcel Error",
-		url: "https://i.ibb.co/TBv3gf8/1.jpg",
-		wishList: false,
-		price: {
-			s: 20,
-			m: 33,
-			l: 100,
-		},
-	},
-	{
-		name: "Supportive Teammate",
-		url: "https://i.ibb.co/B4BfBWD/2.jpg",
-		wishList: false,
-		price: {
-			s: 13,
-			m: 27,
-			l: 45,
-		},
-	},
-	{
-		name: "Undefined",
-		url: "https://i.ibb.co/pWVBqmn/3.jpg",
-		wishList: false,
-		price: {
-			s: 8,
-			m: 30,
-			l: 45,
-		},
-	},
-	{
-		name: "Typescript",
-		url: "https://i.ibb.co/vzgHDBr/4.jpg",
-		wishList: false,
-		price: {
-			s: 15,
-			m: 35,
-			l: 60,
-		},
-	},
-	{
-		name: "Skipped Feature-Branch",
-		url: "https://i.ibb.co/fxJVc9v/5.jpg",
-		wishList: false,
-		price: {
-			s: 100,
-			m: 200,
-			l: 300,
-		},
-	},
-	{
-		name: "Type: any",
-		url: "https://images.pexels.com/photos/4588027/pexels-photo-4588027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		wishList: false,
-		price: {
-			s: 19,
-			m: 37,
-			l: 45,
-		},
-	},
-	{
-		name: "Structured Code",
-		url: "https://i.ibb.co/WF264XK/7.jpg",
-		wishList: false,
-		price: {
-			s: 23,
-			m: 54,
-			l: 76,
-		},
-	},
-	{
-		name: "Cannot Read Properties Of Null",
-		url: "https://images.pexels.com/photos/4588455/pexels-photo-4588455.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		wishList: false,
-		price: {
-			s: 9,
-			m: 21,
-			l: 32,
-		},
-	},
-	{
-		name: ".splice(i,1)",
-		url: "https://images.pexels.com/photos/4588441/pexels-photo-4588441.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		wishList: false,
-		price: {
-			s: 105,
-			m: 267,
-			l: 303,
-		},
-	},
-	{
-		name: "Scrum Master",
-		url: "https://i.ibb.co/6tLQKsB/10.jpg",
-		wishList: false,
-		price: {
-			s: 12,
-			m: 27,
-			l: 33,
-		},
-	},
-	{
-		name: "Passed A Cypress Test",
-		url: "https://images.pexels.com/photos/4580745/pexels-photo-4580745.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-		wishList: false,
-		price: {
-			s: 4,
-			m: 16,
-			l: 20,
-		},
-	},
+  {
+    name: "Merge Conflicts",
+    url: "https://i.ibb.co/1fKFDcB/image.jpg",
+    wishList: false,
+    price: {
+      s: 10,
+      m: 20,
+      l: 30,
+    },
+  },
+  {
+    name: "Parcel Error",
+    url: "https://i.ibb.co/TBv3gf8/1.jpg",
+    wishList: false,
+    price: {
+      s: 20,
+      m: 33,
+      l: 100,
+    },
+  },
+  {
+    name: "Supportive Teammate",
+    url: "https://i.ibb.co/B4BfBWD/2.jpg",
+    wishList: false,
+    price: {
+      s: 13,
+      m: 27,
+      l: 45,
+    },
+  },
+  {
+    name: "Undefined",
+    url: "https://i.ibb.co/pWVBqmn/3.jpg",
+    wishList: false,
+    price: {
+      s: 8,
+      m: 30,
+      l: 45,
+    },
+  },
+  {
+    name: "Typescript",
+    url: "https://i.ibb.co/vzgHDBr/4.jpg",
+    wishList: false,
+    price: {
+      s: 15,
+      m: 35,
+      l: 60,
+    },
+  },
+  {
+    name: "Skipped Feature-Branch",
+    url: "https://i.ibb.co/fxJVc9v/5.jpg",
+    wishList: false,
+    price: {
+      s: 100,
+      m: 200,
+      l: 300,
+    },
+  },
+  {
+    name: "Type: any",
+    url: "",
+    wishList: false,
+    price: {
+      s: 19,
+      m: 37,
+      l: 45,
+    },
+  },
+  {
+    name: "Structured Code",
+    url: "https://i.ibb.co/WF264XK/7.jpg",
+    wishList: false,
+    price: {
+      s: 23,
+      m: 54,
+      l: 76,
+    },
+  },
+  {
+    name: "Cannot Read Properties Of Null",
+    url: "",
+    wishList: false,
+    price: {
+      s: 9,
+      m: 21,
+      l: 32,
+    },
+  },
+  {
+    name: ".splice(i,1)",
+    url: "",
+    wishList: false,
+    price: {
+      s: 105,
+      m: 267,
+      l: 303,
+    },
+  },
+  {
+    name: "Scrum Master",
+    url: "https://i.ibb.co/6tLQKsB/10.jpg",
+    wishList: false,
+    price: {
+      s: 12,
+      m: 27,
+      l: 33,
+    },
+  },
+  {
+    name: "Passed A Cypress Test",
+    url: "",
+    wishList: false,
+    price: {
+      s: 4,
+      m: 16,
+      l: 20,
+    },
+  },
 ];
 
 export interface Iprice {
-	s: number;
-	m: number;
-	l: number;
+  s: number;
+  m: number;
+  l: number;
 }
 
 export interface Iproducts {
-	name: string;
-	url: string;
-	wishList: boolean;
-	price: Iprice;
+  name: string;
+  url: string;
+  wishList: boolean;
+  price: Iprice;
 }
 
 export class StorageProduct {
-	Iproduct: Iproducts;
-	size: string;
-	amount: number;
-	constructor(Iproduct: Iproducts, size: string, amount: number) {
-		this.Iproduct = Iproduct;
-		this.size = size;
-		this.amount = amount;
-	}
+  Iproduct: Iproducts;
+  size: string;
+  amount: number;
+  constructor(Iproduct: Iproducts, size: string, amount: number) {
+    this.Iproduct = Iproduct;
+    this.size = size;
+    this.amount = amount;
+  }
 }
